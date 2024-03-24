2edd11056a402ca04fa0f123149f9e9caf6f3dc3
arthur.escriou <arthur.escriou@gmail.com>
2024-03-24 17:07:48 +0100

    create bash

---
+++ createAll.sh
@@ -0,0 +1,4 @@
+kubectl create -f node-redis.deployment.yml
+kubectl create -f redis.deployment.yml
+kubectl create -f node-redis.service.yml
+kubectl create -f redis.service.yml
---
+++ deleteAll.sh
@@ -0,0 +1,4 @@
+kubectl delete -f node-redis-deployment.yml
+kubectl delete -f redis-deployment.yml
+kubectl delete -f node-redis-service.yml
+kubectl delete -f redis-service.yml
